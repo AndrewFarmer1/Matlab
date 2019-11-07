39e8d067720030804c5b6f05c03f8a74dfe00da9
Andrew Farmer <Regularshowrulez@gmail.com>
2019-11-06 18:31:13 -0600

    Updated versions using only the required two text reads

--- assign08a.m
+++ assign08a.m
@@ -2,11 +2,12 @@
   Assign08a.m
   Submitter's name: Andrew Farmer
   Group Members: Lane Jemison, Sarah Pepper, Alexis Jackson
-  Group Number:
+  Group Number: 100
   Due date: 11/8/19
   Project Summary: This program reads the stats for both Auburn and their
   opponent and prints a report and a graph
-  Collboration Statement: 
+  Collboration Statement: In order to complete this project we referred to
+  class notes and used the in class slides and exmaples
 %}
 clc, clear all
 %*****CONSTANTS*****
@@ -15,24 +16,26 @@ OPP_FILE_NAME = 'opp_stats08.txt';
 %*****INPUTS*****
 %If and only if both of the files exist
 if exist(AU_FILE_NAME, 'file') && exist(OPP_FILE_NAME, 'file')
-%Read the textfile and save the month and date in a 2x2 for Auburn
-    [au_Game_Date_Month_And_Day(:,1),au_Game_Date_Month_And_Day(:,2)] = textread(AU_FILE_NAME, '%d%d%*d%*d%*d%*d%*d%*d%*d%*d');
-%Read the entire data file and only save the rushing data in 4x4 for Auburn
-    [au_Rushing_Stats(:,1),au_Rushing_Stats(:,2),au_Rushing_Stats(:,3),au_Rushing_Stats(:,4)] = textread(AU_FILE_NAME,'%*d%*d%d%d%d%d%*d%*d%*d%*d');
-%Read the entire data file and only save the passing Stats in 4x4 for auburn
-    [au_Passing_Stats(:,1),au_Passing_Stats(:,2),au_Passing_Stats(:,3),au_Passing_Stats(:,4)] = textread(AU_FILE_NAME,'%*d%*d%*d%*d%*d%*d%d%d%d%d');
-%Read the date for the opponent in a 2x2 matrix
-    [opp_Game_Month_Date(:,1),opp_Game_Month_Date(:,2)] =  textread(OPP_FILE_NAME, '%d%d%*d%*d%*d%*d%*d%*d%*d%*d');
-%Save the rushing data for the opponents in a 4x4 matrix
-    [opp_Rushing_Stats(:,1),opp_Rushing_Stats(:,2),opp_Rushing_Stats(:,3),opp_Rushing_Stats(:,4)] =textread(OPP_FILE_NAME,'%*d%*d%d%d%d%d%*d%*d%*d%*d');
+%Read the textfile and save the Game data for Auburn
+   [au_Game_Stats(:,1),au_Game_Stats(:,2),au_Game_Stats(:,3),au_Game_Stats(:,4),au_Game_Stats(:,5),au_Game_Stats(:,6),au_Game_Stats(:,7),au_Game_Stats(:,8),au_Game_Stats(:,9),au_Game_Stats(:,10)] = textread(AU_FILE_NAME, '%d%d%d%d%d%d%d%d%d%d');
+%Read the entire data file and save the game data for the Opponents 
+   [opp_Game_Stats(:,1),opp_Game_Stats(:,2),opp_Game_Stats(:,3),opp_Game_Stats(:,4),opp_Game_Stats(:,5),opp_Game_Stats(:,6),opp_Game_Stats(:,7),opp_Game_Stats(:,8),opp_Game_Stats(:,9),opp_Game_Stats(:,10)] = textread(OPP_FILE_NAME,'%d%d%d%d%d%d%d%d%d%d');
+%Read the Auburn Game data and save only month and date in a 2x2
+   au_Game_Date_Month_And_Day = au_Game_Stats(:,(1:2));
+%Read the Auburn data and only save the passing Stats in 4x4 matrix
+    au_Rushing_Stats = au_Game_Stats(:,(3:6));
+%Create a 4x4 matrix of Auburns Rushing stats from Auburn data
+    au_Passing_Stats = au_Game_Stats(:,(7:10));
+%Read the Opp game data and save the month and date in 2x2 matrix
+    opp_Game_Month_Date = opp_Game_Stats(:,(1:2));
+%read the opp game data and save the the rushing stats in 4x4 matrix
+    opp_Rushing_Stats = opp_Game_Stats(:,(3:6));
 %Save the Passing stats for the opponents in a 4x4 matrix
-    [opp_Passing_Stats(:,1),opp_Passing_Stats(:,2),opp_Passing_Stats(:,3),opp_Passing_Stats(:,4)] = textread(OPP_FILE_NAME,'%*d%*d%*d%*d%*d%*d%d%d%d%d');
+    opp_Passing_Stats = opp_Game_Stats(:,(7:10));
 %*****COMPUTE*****
-%Determine the number of games played by the Opponent
-opp_Num_Games = length(opp_Game_Month_Date');
-%Calculate per attempt yardage and add it to a new column in the old matrix
+%Calculate rushing per attempt yardage and add it to a new column in the old matrix
 au_Rushing_Stats(:,5) = au_Rushing_Stats(:,2) ./ au_Rushing_Stats(:,1);
-%Calculate the Passing Yards per attempt for the Opponent and add it to old
+%Calculate the Passing Yards per attempt for the Auburn and add it to old
 %matrix
 au_Passing_Stats(:,5) = au_Passing_Stats(:,2)./ au_Passing_Stats(:,1);
 %Calculate the Rushing yards per attmept for the Opponent and add to old
@@ -44,7 +47,7 @@ opp_Passing_Stats(:,5) = opp_Passing_Stats(:,2)./ opp_Passing_Stats(:,1);
 %Call the outputReportGraph function using the Auburn data
 outputReportGraph(au_Game_Date_Month_And_Day,au_Rushing_Stats,au_Passing_Stats,'Auburn',1)
 %Call the outputReportGraph using the opponent data
-outputReportGraph(opp_Game_Month_Date,opp_Rushing_Stats,opp_Passing_Stats,'Opponent',2)
+outputReportGraph(opp_Game_Month_Date,opp_Rushing_Stats,opp_Passing_Stats,'Opponents',2)
 %*****OUTPUT*****
 %if one of the files does not exist produce an error
 else
--- outputReportGraph.m
+++ outputReportGraph.m
@@ -2,11 +2,12 @@
   Function outputReportGraph used in Assign08a.m
   Submitter's name: Andrew Farmer
   Group Members: Lane Jemison, Sarah Pepper, Alexis Jackson
-  Group Number:
-  Due date: 11/8/19
+  Group Number: 100
+  Due date: 11/6/19
   Project Summary: This function is used by assign08. m in order to output
   the report and graph of both Auburn's and their opponents football stats
-  Collboration Statement: 
+  Collboration Statement: In order to complete this project we referred to
+  class notes and used the in class slides and exmaples
 %}
 function [] = outputReportGraph (date, rushing, passing, name, figureNum)
 %print stat report and draw graph
@@ -16,12 +17,15 @@ fprintf('Date  Att Yds per Att  Att Yds per Att \n')
 %For every game played print out the stats for both teams
 for i = 1: length(date')
 %print the date in the specfied format
-fprintf('%2.0d/%2.0d  ',[date(i,1);date(i,2)])
+fprintf('%02d/%02d  ',[date(i,1);date(i,2)])
 %print the rushing yards with the computed average
 fprintf('%2.0f %3.0f %5.2f',rushing(i,1),rushing(i,2),rushing(i,5))
 %Print the passing yards with the computed average
 fprintf('     %2.0f %3.0f  %5.2f \n',passing(i,1),passing(i,2),passing(i,5))
 end
+%Add an extra line space so the text matches the example and is more
+%readable
+fprintf('\n')
 %Save the graph title as variable name so that its modular
 titleText = sprintf('2019 Season stats for %s', name);
 %the figure command makes the graph pop up in front of opened windows
